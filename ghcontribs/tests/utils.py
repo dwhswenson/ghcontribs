ff0387ec9cd6c8f2acca8f5c99563f2bee250278
David W.H. Swenson <dwhs@hyperblazer.net>
2021-11-11 19:22:23 -0500

    add coverage exclusion info

--- ghcontribs/tests/utils.py
+++ ghcontribs/tests/utils.py
@@ -3,13 +3,13 @@ import pytest
 
 try:
     urllib.request.urlopen('https://www.google.com')
-except:
+except:  # -no-cov-
     HAS_INTERNET = False
 else:
     HAS_INTERNET = True
 
 def assert_url(url):
-    if not HAS_INTERNET:
+    if not HAS_INTERNET:  # -no-cov-
         pytest.skip("Internet connection seems faulty")
 
     resp = urllib.request.urlopen(url)
